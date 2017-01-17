5dadb61e36a6ed66a938615dff3fc01d421fb44e
Mark B. Sigler <markbsigler@gmail.com>
2017-01-17 17:57:31 -0500

    touch dir and all files

--- touchrelease.sh
+++ touchrelease.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+bnm=""
 reldt=""
 time="0000.00"
 for file in $(find . -maxdepth 2 -type f -name *.nfo)
@@ -6,10 +7,11 @@ do
 	reldt=$(grep "releasedate" $file | sed -e 's/<releasedate>//' -e 's/<\/releasedate>//' | sed -e 's/-//g')
 	if [ -n "$reldt" ]
 	then
-		touch -c -t $reldt$time $file
+		touch -c -t $reldt$time $(dirname $file)
+		touch -c -t $reldt$time $(dirname $file)/*
 		reldt=""
-	else
-		echo "No release date or nfo for: $file"
+#	else
+#		echo "No release date or nfo for: $file"
 	fi
 done
 
